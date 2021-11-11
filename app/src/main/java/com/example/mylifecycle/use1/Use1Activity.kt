cb278230fe745dbf616178e22e2d8de5eb946343
guandi <guandi@smartens-tek.com>
2021-11-11 09:40:15 +0800

    # lifecycle 6种基础用法

--- app/src/main/java/com/example/mylifecycle/use1/Use1Activity.kt
+++ app/src/main/java/com/example/mylifecycle/use1/Use1Activity.kt
@@ -5,10 +5,17 @@ import android.os.Bundle
 import com.example.mylifecycle.R
 
 /**
+ *
+ * Lifecycle可以有效的避免内存泄漏和解决android生命周期的常见难题
+ * Lifecycle 是一个表示android生命周期及状态的对象
+ * LifecycleOwner 用于连接有生命周期的对象，如activity,fragment
+ * LifecycleObserver 用于观察查LifecycleOwner
+ * Lifecycle框架使用观察者模式实现观察者监听被观察者的生命周期的变化
  * 第一個版本(监听器)
  */
 class Use1Activity : AppCompatActivity() {
     private lateinit var myListener: MyListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_use1)
@@ -24,7 +31,7 @@ class Use1Activity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-        if (this::myListener.isInitialized){
+        if (this::myListener.isInitialized) {
             myListener.stop()
         }
     }
